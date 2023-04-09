2b5b3efdf96317c918771b6d0d7d4a23550f0a01
Han <hander.juan@gmail.com>
2023-04-09 17:08:12 +0200

    distri

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -4,7 +4,7 @@ import { MarketingApp } from "./pages/marketing";
 export default function App() {
   return (
     <>
-      <h1>Hello theressssssdasdadasssss</h1>
+      <h1>Hello theressssssdasdadassssssssss</h1>
       <hr />
       <MarketingApp />
     </>
