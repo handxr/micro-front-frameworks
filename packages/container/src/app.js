66657ba939ffacdde7c9665dbcd66549116ad407
Han <hander.juan@gmail.com>
2023-04-09 17:35:22 +0200

    adding typo

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/header";
 import { MarketingApp } from "./pages/marketing";
 
 export default function App() {
