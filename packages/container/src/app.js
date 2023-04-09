f09241ad9e4fdd284a82c3c0b6a5fa800ae741bc
Han <hander.juan@gmail.com>
2023-04-09 17:43:35 +0200

    fixing css

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
 import Header from "./components/header";
 import { MarketingApp } from "./pages/marketing";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider injectFirst generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 }
--- packages/marketing/src/app.js
+++ packages/marketing/src/app.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Landing from "./pages/landing";
 import Pricing from "./pages/pricing";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 export default function App() {
   return (
-    <StylesProvider injectFirst>
+    <StylesProvider injectFirst generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Landing} />
