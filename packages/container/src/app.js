b400ae3292c16708c9356be7140da912452713c2
Han <hander.juan@gmail.com>
2023-04-09 16:22:21 +0200

    fixing

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -4,7 +4,7 @@ import { MarketingApp } from "./pages/marketing";
 export default function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Hello there</h1>
       <hr />
       <MarketingApp />
     </>
